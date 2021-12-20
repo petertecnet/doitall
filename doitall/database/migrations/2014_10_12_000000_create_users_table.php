089621846966823ff1617ac81e637a9fb3608b27
Pedro <pivow@hotmail.com>
2021-12-20 19:15:52 -0300

    Lixo

--- doitall/database/migrations/2014_10_12_000000_create_users_table.php
+++ doitall/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->tinyInteger('status')->default(1)->comment('0 bloqueado - 1 liberado - 2 pendente');
-            $table->tinyInteger('type')->default(0)->comment('0-Cliente - 1-Funcionario - 2-Supervisor 8-Gerente 9-Administrador');
+            $table->tinyInteger('role')->default(0)->comment('0-Cliente - 1-Funcionario - 2-Supervisor 8-Gerente 9-Administrador');
             $table->string('name',50);
             $table->string('email',150)->unique();
             $table->timestamp('email_verified_at')->nullable();
@@ -45,12 +45,12 @@ class CreateUsersTable extends Migration
         Schema::dropIfExists('users');
     }
 
-    public function newADM($name,$user,$pass,$type=9){
+    public function newADM($name,$user,$pass,$role=9){
         $cad = new User();
         $cad->name = $name;
         $cad->email = $user;
         $cad->password = bcrypt($pass);
-        $cad->type = $type;
+        $cad->role = $role;
         $cad->status = 1;
         $cad->save();
     }
--- doitall/routes/web.php
+++ doitall/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Auth::routes();
+Route::get('/', function () {return view('welcome');})->name('welcome');
+
+Route::group(['middleware' => 'auth'], function () {
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+});
