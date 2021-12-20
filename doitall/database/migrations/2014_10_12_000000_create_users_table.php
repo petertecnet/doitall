a0704363f48cc15775cdb62bcc724d03277f4d28
Pedro <pivow@hotmail.com>
2021-12-20 17:25:19 -0300

    Migrate de uers

--- doitall/database/migrations/2014_10_12_000000_create_users_table.php
+++ doitall/database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,15 +15,25 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->bigIncrements('id');
+            $table->tinyInteger('status')->default(1)->comment('0 bloqueado - 1 liberado - 2 pendente');
+            $table->tinyInteger('type')->default(0)->comment('0-Cliente - 1-Funcionario - 2-Supervisor 8-Gerente 9-Administrador');
+            $table->string('name',50);
+            $table->string('email',150)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->uuid('uid')->nullable();
+            //
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
-    }
+
+        $this->newADM('Fabio','fabio@doitall.com.br','123456');
+        $this->newADM('Pedro','pivow@doitall.com.br','123456');
+        $this->newADM('Lucas', 'lucas@doitall.com', '123456');
+}
+
 
     /**
      * Reverse the migrations.
@@ -33,4 +44,15 @@ class CreateUsersTable extends Migration
     {
         Schema::dropIfExists('users');
     }
+
+    public function newADM($name,$user,$pass,$type=9){
+        $cad = new User();
+        $cad->name = $name;
+        $cad->email = $user;
+        $cad->password = bcrypt($pass);
+        $cad->type = $type;
+        $cad->status = 1;
+        $cad->save();
+    }
+
 }
