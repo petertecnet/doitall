f3817873fd4531f885764ba1e9c37e8cbcfd6cea
Pedro <pivow@hotmail.com>
2021-12-21 00:35:55 -0300

    CRUD Company

--- doitall/routes/web.php
+++ doitall/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CompanyController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => 'auth.role:9'], function () {
+
+    Route::resources([
+        'companies' => CompanyController::class,
+    ]);
+
+});
